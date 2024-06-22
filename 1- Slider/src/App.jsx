275aaae014676fe94070728204c8432e0b23c063
rabeehcv <rabeehcv28@gmail.com>
2024-06-22 16:13:36 +0530

    Update App.jsx

--- 1- Slider/src/App.jsx
+++ 1- Slider/src/App.jsx
@@ -1,14 +1,33 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import items from './data';
 import { ImQuotesRight } from "react-icons/im";
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import SingleItem from './component';
 
 function App(){
-const [people,setPeople] = useState(items);
-const [index,setIndex] = useState(0);
+  const [people,setPeople] = useState(items);
+  const [index,setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex=people.length-1;
+    if(index<0){
+      setIndex(lastIndex);
+    }
+    if(index>lastIndex){
+      setIndex(0);
+    }
+  }, [index,people] );
+
+  useEffect(()=>{
+    let slider = setInterval(()=>{
+      setIndex(index+1);
+    },3000);
+    return ()=> clearInterval(slider);
+  },[index]);
+
 
   return(
-    <section>
+    <section className='section'>
       <div className='title'>
         <h2>
           <span>/</span>Reviews - And the Mountains Echoed
@@ -16,10 +35,17 @@ const [index,setIndex] = useState(0);
         <div className='underline'></div>
       </div>
       <div className='section-center'>
-        {people.map((person)=> {
+        {people.map((person,personIndex)=> {
           const {id,image,name,title,review} = person;
+          let position="nextSlide";
+          if(personIndex===index){
+            position='activeSlide';
+          }
+          if(personIndex===index-1 || (index===0 && personIndex===people.length-1)){
+            position='lastSlide';
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={title} className='person-img'/>
               <h4>{name}</h4>
               <p className='title'>{title}</p>
@@ -30,6 +56,12 @@ const [index,setIndex] = useState(0);
             </article>
           )
         })}
+        <button className="prev" onClick={()=>setIndex(index-1)}>
+          <FiChevronLeft />
+        </button>
+        <button className="next" onClick={()=>setIndex(index+1)}>
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   );
